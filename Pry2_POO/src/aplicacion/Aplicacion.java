da99d7d175d44ea06c58906dfa31989ffa640ab9
Andy Porras <andyporras@estudiantec.cr>
2022-06-08 15:27:29 -0600

    Cambio en los nombres de los paquetes

---
+++ Pry2_POO/src/aplicacion/Aplicacion.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplicacion;
+
+/**
+ *
+ * @author Andy Porras
+ */
+public class Aplicacion {
+    
+        /**
+        * @param args the command line arguments
+        */
+        public static void main(String[] args) {
+            String[][] matriz = new String [5][5];
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    matriz[i][j] = 23 + "";
+                }
+            }
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    System.out.print(matriz[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+        
+    
+}
---
+++ Pry2_POO/src/control/Tombola.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control;
+
+import java.util.ArrayList;
+import modelo.Bola;
+
+/**
+ *
+ * @author Ing. Kevin
+ */
+public class Tombola {
+  private ArrayList<Bola> bolas;   
+  
+  public Tombola(){
+      
+  }
+}
