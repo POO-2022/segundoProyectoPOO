473edd89ea77285ee9e9a60cd017a83a2a3505ea
DerrickAAU <80082136+DerrickAAU@users.noreply.github.com>
2022-06-12 10:52:36 -0600

    Detalles y creación de ventana
    Creación de ventana para jugar y arreglo de detalles

--- Pry2_POO/src/vista/IniciarJuego.java
+++ Pry2_POO/src/vista/IniciarJuego.java
@@ -34,6 +34,7 @@ public class IniciarJuego extends javax.swing.JFrame {
         btJugarBingo = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         premioDelJuego = new javax.swing.JTextField();
+        btRegresarMenuPrincipal = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -67,6 +68,11 @@ public class IniciarJuego extends javax.swing.JFrame {
 
         premioDelJuego.setText("Premio");
 
+        btRegresarMenuPrincipal.setBackground(new java.awt.Color(255, 51, 51));
+        btRegresarMenuPrincipal.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        btRegresarMenuPrincipal.setForeground(new java.awt.Color(153, 0, 0));
+        btRegresarMenuPrincipal.setText("Regresar");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -91,7 +97,10 @@ public class IniciarJuego extends javax.swing.JFrame {
                         .addComponent(premioDelJuego, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(252, 252, 252)
-                        .addComponent(btJugarBingo)))
+                        .addComponent(btJugarBingo))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btRegresarMenuPrincipal)))
                 .addContainerGap(32, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -108,18 +117,20 @@ public class IniciarJuego extends javax.swing.JFrame {
                     .addComponent(premioDelJuego, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(40, 40, 40)
                 .addComponent(btJugarBingo)
-                .addContainerGap(38, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btRegresarMenuPrincipal)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -162,6 +173,7 @@ public class IniciarJuego extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btJugarBingo;
+    public javax.swing.JButton btRegresarMenuPrincipal;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
