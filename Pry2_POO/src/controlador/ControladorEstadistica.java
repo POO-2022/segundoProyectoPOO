4410da3fd1c6495ffd56bc8ecc1d8fee8e2e737b
Kevin Salazar <80077213+Kevin-Salazar-itcr@users.noreply.github.com>
2022-06-19 13:08:52 -0600

    Arreglo de parametrizacion
    La clase ControladorEstadistica esta lista para usarse, solo falta el traspaso de datos desde el xml
    la clase posee 3 arrays con las iniciales DB (dataBase), cada una de tipo Statistic
    la clase statistic cuenta con 3 parametros: dato: int, dato_aux: String (para las veces que se requiera un dato string) y cant: int (para las veces que se repite un dato)

--- Pry2_POO/src/controlador/ControladorEstadistica.java
+++ Pry2_POO/src/controlador/ControladorEstadistica.java
@@ -12,6 +12,7 @@ import vista.MenuPrincipal;
 import vista.Estadistica;
 import modelo.Bingo;
 import modelo.Carton;
+import modelo.Statistic;
 import modelo.Jugador;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -25,19 +26,36 @@ import org.jfree.data.general.DefaultPieDataset;
  * @version 1.0
  */
 public class ControladorEstadistica implements ActionListener{
+  public ArrayList<Statistic> DBGanadores;  
+  public ArrayList<Statistic> DBNumeros; 
+  public ArrayList<Statistic> DBPartidas;  
   public Estadistica vista;
-  public ArrayList<Jugador> jugadores;
-  public ArrayList<Carton> cartones;
+  public ArrayList<Jugador> top5Ganadores; //se pueden recuperar de la base de datos csv
+  public ArrayList<Integer> top10Nums;
+  public ArrayList<String> cantPartidas;
   public MenuPrincipal vistaAnterior;
+  public JFreeChart grafico;
   
-  public ControladorEstadistica(Estadistica pVistaEstadistica, ArrayList<Jugador> pJugadores,
-      ArrayList<Carton> pCartones, MenuPrincipal pVistaAnterior) {
-    this.vista = pVistaEstadistica;
-    this.jugadores = pJugadores;
-    this.cartones = pCartones;
+  /**
+   * 
+   * @param pVistaEstadistica
+   * @param pVistaAnterior la ventana previa a la que se abrió
+   * @param t5g lista con el historial de  ganado
+   * @param t10n lista con todos los numeros más cantados
+   * @param cpt lista con un historial de configuracion de partidas
+   */
+  public ControladorEstadistica(Estadistica pVistaEstadistica, MenuPrincipal pVistaAnterior, 
+          ArrayList<Jugador> t5g, ArrayList<Integer> t10n, ArrayList<String> cpt) {
+    this.vista = pVistaEstadistica; 
     this.vistaAnterior = pVistaAnterior;
     this.vista.btConsultar.addActionListener(this);
     this.vista.btVolver.addActionListener(this);
+    this.top10Nums = t10n;
+    this.top5Ganadores = t5g;
+    this.cantPartidas = cpt;
+    this.DBNumeros = new ArrayList<Statistic>();
+    this.DBGanadores = new ArrayList<Statistic>();
+    this.DBPartidas = new ArrayList<Statistic>();
   }
   @Override
   public void actionPerformed(ActionEvent e) {
@@ -46,7 +64,12 @@ public class ControladorEstadistica implements ActionListener{
         cerrarVentanaEstadistica();
         break;
       case "Consultar": 
-        generarConsulta();  
+        try{  
+          generarConsulta();  
+        }
+        catch(Exception exc){
+            JOptionPane.showMessageDialog(null, "Error: datos insuficientes...");
+        }
         break;
       default:
         break;
@@ -55,71 +78,75 @@ public class ControladorEstadistica implements ActionListener{
   
   public void generarConsulta(){
       String tipo = vista.getTipoConsulta();
-      JFreeChart grafico = null;
-      DefaultCategoryDataset datos = new DefaultCategoryDataset();
-      int d1 = 210;
-      int d2 = 65;
-      int d3 = 84;
-      int d4 = 154;
-      int d5 = 55;
-      int d6 = 75;
-      int d7 = 525;
-      int d8 = 0;
-      int d9 = 57;
-      int d10 = 14;
-      datos.addValue(d1, "Grafica 1", "Uno");
-      datos.addValue(d2, "Grafica 1", "Dos");
-      datos.addValue(d3, "Grafica 1", "Tres");
-      datos.addValue(d4, "Grafica 1", "Cuatro");
-      datos.addValue(d5, "Grafica 1", "Cinco");
-      datos.addValue(d6, "Grafica 1", "seis");
-      datos.addValue(d7, "Grafica 1", "siete");
-      datos.addValue(d8, "Grafica 1", "ocho");
-      datos.addValue(d9, "Grafica 1", "nueve");
-      datos.addValue(d10, "Grafica 1", "diez");
-      
       if(tipo.equals("Números más cantados")){  
-        grafico = ChartFactory.createBarChart("Grafica Prueba", "Eje X", "Eje Y", datos, PlotOrientation.VERTICAL, true, true, false);
+        numsMasCantados();
+        return;
       }
       if(tipo.equals("Historial config de partidas")){
-        DefaultPieDataset pastel = new DefaultPieDataset();
-        pastel.setValue("Uno", d1);
-        pastel.setValue("Dos", d2);
-        pastel.setValue("Tres", d3);
-        pastel.setValue("Cuatro", d4);
-        pastel.setValue("Cinco",d5);
-        pastel.setValue("seis",d6);
-        pastel.setValue("siete",d7);
-        pastel.setValue("ocho",d8);
-        pastel.setValue("nueve",d9);
-        pastel.setValue("diez",d10);
-        grafico = ChartFactory.createPieChart("GraficaPrueba", pastel, true, true, false);  
+        histConfigPartidas(); 
+        return;
       }
       if(tipo.equals("Cantidad de partidas por fecha")){
-        JOptionPane.showMessageDialog(null, "Opción no disponible...\nDisculpe los inconvenientes", "Atención", JOptionPane.WARNING_MESSAGE);
+        cantPartXFecha();
         return;
       }
       if(tipo.equals("Top 5 ganadores")){
-        DefaultPieDataset pastel = new DefaultPieDataset();
-        pastel.setValue("Uno", d1);
-        pastel.setValue("Dos", d2);
-        pastel.setValue("Tres", d3);
-        pastel.setValue("Cuatro", d4);
-        pastel.setValue("Cinco",d5);
-        pastel.setValue("seis",d6);
-        pastel.setValue("siete",d7);
-        pastel.setValue("ocho",d8);
-        pastel.setValue("nueve",d9);
-        pastel.setValue("diez",d10);
-        grafico = ChartFactory.createPieChart3D("GraficaPrueba", pastel, true, true, false);  
+        top5Ganadores();
+        return;
       }
-      ChartPanel cPanel = new ChartPanel(grafico);
-      JFrame informacion = new JFrame("Grafica");
-      informacion.getContentPane().add(cPanel);
-      informacion.pack();
-      informacion.setVisible(true); 
-      informacion.setLocationRelativeTo(null);
+      return;
   }
+  
+  //estadistica de los 10 numeros mas cantados en partidas
+  public void numsMasCantados(){
+    DefaultCategoryDataset datos = new DefaultCategoryDataset();
+    for(int i = 0; i<DBNumeros.size(); i++){
+        datos.addValue(DBNumeros.get(i).getCant(), "Grafica 1", DBNumeros.get(i).getDato_aux());
+    }grafico = ChartFactory.createBarChart("Grafica Prueba", "Eje X", "Eje Y", datos, PlotOrientation.VERTICAL, true, true, false);
+    mostrar(grafico);
+    return;
+  }
+  
+  //historial de cuantas partidas de cada tipo se han hecho
+  public void histConfigPartidas(){
+    DefaultCategoryDataset datos = new DefaultCategoryDataset();
+    DefaultPieDataset pastel = new DefaultPieDataset();
+    for(int i = 0; i<DBPartidas.size(); i++){
+        datos.addValue(DBPartidas.get(i).getCant(), "Grafica 1", DBPartidas.get(i).getDato_aux());
+    }
+    grafico = ChartFactory.createPieChart("GraficaPrueba", pastel, true, true, false);
+    mostrar(grafico);
+    return;
+  }
+  
+  //estadistica de cantidad de partidas hechas por fecha
+  //sin implementar...
+  public void cantPartXFecha(){
+    JOptionPane.showMessageDialog(null, "Opción no disponible...\nDisculpe los inconvenientes", "Atención", JOptionPane.WARNING_MESSAGE);
+    return;
+  }
+  
+  //top 5 ganadores
+  public void top5Ganadores(){   
+    DefaultPieDataset pastel = new DefaultPieDataset();
+    for(int i = 0; i<DBGanadores.size(); i++){
+        pastel.setValue(DBGanadores.get(i).getDato_aux(), DBGanadores.get(i).getCant());
+    }
+    grafico = ChartFactory.createPieChart3D("GraficaPrueba", pastel, true, true, false);
+    mostrar(grafico);
+    return;
+  }
+  
+  //muestra el gráfico en pantalla
+  public void mostrar(JFreeChart pGrafico){
+    ChartPanel cPanel = new ChartPanel(pGrafico);
+    JFrame informacion = new JFrame("Grafica");
+    informacion.getContentPane().add(cPanel);
+    informacion.pack();
+    informacion.setVisible(true); 
+    informacion.setLocationRelativeTo(null);
+  }
+  
   
   private void cerrarVentanaEstadistica() {
     this.vista.setVisible(false);
